b6824fd19d75f4f00cacde8be0ee98f86fa80a61
Mayur Karad <mayurkarad721@gmail.com>
2026-03-13 22:55:19 +0530

    Time: 3 ms (31.15%), Space: 47.2 MB (64.3%) - LeetHub

---
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int sum =0;
+        for(int n : nums){
+            min = Math.min(min,n);
+            sum+=n;
+        }
+        return sum-min*nums.length;
+    }
+}
